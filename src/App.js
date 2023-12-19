7573b4a1434a37ee976c11f33d0cf2a6e89a5512
JaanMun <yjm7575@naver.com>
2023-12-20 00:39:11 +0900

    [feat] Add logic for game behavior

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Board from './components/Board';
 function App() {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
 
   const calculateWinner = squares => {
     const lines = [
@@ -27,7 +28,7 @@ function App() {
     return null;
   };
 
-  const currentHistory = history.at(-1);
+  const currentHistory = history[stepNumber];
   const winner = calculateWinner(currentHistory.squares);
 
   let status;
@@ -38,22 +39,48 @@ function App() {
   }
 
   const handleClick = i => {
-    const newSquares = currentHistory.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrentHistory = newHistory[newHistory.length - 1];
+    const newSquares = newCurrentHistory.squares.slice();
     if (calculateWinner(newSquares) || newSquares[i]) return;
 
     newSquares[i] = xIsNext ? 'X' : 'O';
-    setHistory([...history, { squares: newSquares }]);
+    setHistory([...newHistory, { squares: newSquares }]);
     setXIsNext(current => !current);
+    setStepNumber(newHistory.length);
   };
 
+  const jumpTo = step => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start';
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
   return (
-    <div className="game">
-      <div className="status">{status}</div>
-      <div className="game-board">
-        <Board squares={currentHistory.squares} onClick={i => handleClick(i)} />
+    <>
+      <div className="game">
+        <header className="app-header">
+          <h1>React Tic Tac Toe</h1>
+        </header>
+        <div className="game-board">
+          <Board squares={currentHistory.squares} onClick={i => handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div className="status">{status}</div>
+          <ol>{moves}</ol>
+        </div>
       </div>
-      <div className="game-info">game-info</div>
-    </div>
+    </>
   );
 }
 
